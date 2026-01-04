d0cc0aec95c07fd745574c9d85ced49b6bfcdd8c
chijw <2922234217@qq.com>
2026-01-04 14:57:56 +0800

    add copilot

--- init.lua
+++ init.lua
@@ -868,7 +868,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -1017,6 +1017,54 @@ require('lazy').setup({
           show_buffer_close_icons = true,
           show_close_icon = true,
         },
+      }
+    end,
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = '<C-l>',
+            accept_word = false,
+            accept_line = false,
+            -- next = '<C-]>',
+            -- prev = '<C-[>',
+            -- dismiss = '<C-:>',
+          },
+        },
+        -- panel = {
+        --   enabled = true,
+        --   auto_refresh = false,
+        --   keymap = {
+        --     jump_prev = '[[',
+        --     jump_next = ']]',
+        --     accept = '<CR>',
+        --     refresh = 'gr',
+        --     open = '<M-p>',
+        --   },
+        --   layout = {
+        --     position = 'bottom',
+        --     ratio = 0.4,
+        --   },
+        -- },
+        -- filetypes = {
+        --   markdown = true,
+        --   help = false,
+        --   gitcommit = false,
+        --   gitrebase = false,
+        --   hgcommit = false,
+        --   svn = false,
+        --   cvs = false,
+        --   ['.'] = false,
+        -- },
+        copilot_node_command = 'node',
       }
     end,
   },
