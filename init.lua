00569f031cd708f60f1034a42efe512f9d078ecc
chijw <2922234217@qq.com>
2026-01-06 19:23:28 +0800

    update

--- init.lua
+++ init.lua
@@ -1,103 +1,12 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-if vim.fn.has 'gui_running' == 0 then
-  pcall(vim.fn.serverstart, '/tmp/nvimsocket')
-end
---
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.clipboard = 'unnamedplus'
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -196,10 +105,10 @@ vim.o.confirm = true
 -- vim.g.vimtex_quickfix_open_on_warning = 0
 -- vim.g.vimtex_quickfix_mode = 0
 
-local first_arg = vim.fn.argv(0)
-if type(first_arg) == 'string' and first_arg ~= '' and vim.fn.isdirectory(first_arg) == 1 then
-  vim.api.nvim_set_current_dir(tostring(first_arg))
-end
+-- local first_arg = vim.fn.argv(0)
+-- if type(first_arg) == 'string' and first_arg ~= '' and vim.fn.isdirectory(first_arg) == 1 then
+--   vim.api.nvim_set_current_dir(tostring(first_arg))
+-- end
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
@@ -336,6 +245,11 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+  },
+
+  {
+    'mg979/vim-visual-multi',
+    branch = 'master',
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -770,7 +684,26 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        -- pyright = {
+        --   settings = {
+        --     pyright = {
+        --       disableOrganizeImports = true,
+        --     },
+        --     python = {
+        --       analysis = {
+        --         ignore = { '*' },
         --
+        --         typeCheckingMode = 'off',
+        --         diagnosticSeverityOverrides = {
+        --           reportUnusedVariable = 'none',
+        --           reportUnusedImport = 'none',
+        --           reportUndefinedVariable = 'none',
+        --         },
+        --       },
+        --     },
+        --   },
+        -- },
+        -- ruff = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -803,6 +736,8 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'pyright',
+        -- 'ruff',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -856,6 +791,11 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        -- python = {
+        --   'ruff_fix',
+        --   'ruff_format',
+        --   'ruff_organize_imports',
+        -- },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
